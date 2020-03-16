88f5a7784ffa874303b12cdbe876792494d4a30a
codegitpro <yusufqing920@gmail.com>
2020-03-17 03:20:04 +0800

    complete project

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,38 @@ const express = require('express');
 
 const { SERVE_HOSTNAME, SERVE_PORT } = require('../src/config.json');
 const cookieSession = require('cookie-session');
-const { v4: uuid } = require('uuid')
+const { v4: uuid } = require('uuid');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const urlController = require('./controllers/url.controller');
 require('dotenv-safe').config();
 
+mongoose.connect(
+  process.env.MONGO_URI, 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  }
+);
+
 const app = express();
+
+app.use(cors({ 
+  origin: 'http://localhost:3000',
+  credentials: true 
+}));
+app.use(bodyParser.json());
 
 app.use(cookieSession({
   name: 'shortlinks',
   keys: [process.env.SESHSECRET],
   maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-}))
-
+}));
 
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.url}`);
   req.session.id = (req.session.id || uuid());
-  res.header('Access-Control-Allow-Origin', '*');
   next(); // pass control to the next handler
 });
 
@@ -30,36 +46,13 @@ app.get('/', (req, res) => {
 })
 
 
-app.get(
-  '/api/links', 
-  (req, res, next)=> {
-    res.json([
-      'test1',
-      'test2'
-    ])
-    // TODO
-    next();
-  }
-)
+app.get('/api/links', urlController.list);
 
-app.post(
-  '/api/links', 
-  (req, res, next)=> {
-    // TODO
-    next();
-  }
-)
+app.post('/api/links', urlController.shorten);
 
+app.get('/api/links/:id', urlController.get);
 
-app.get(
-  '/api/links/:id', 
-  (req, res, next)=> {
-    // TODO
-    res.send(`Id=${req.params.id}`);
-    next();
-  }
-)
-
+app.get('/:id', urlController.redirect);
 
 app.listen(
   SERVE_PORT, 
--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import config from './config.json';
-
-const LINKS_URL = `//${config.SERVE_HOSTNAME}:${config.SERVE_PORT}/api/links`;
+import UrlShortner from './components/UrlShortner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Replace me with shortlinks UI</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <a
-            className="App-link"
-            onClick={()=> {
-              fetch(LINKS_URL).then((resp)=> { 
-                resp.json().then((x)=> alert(JSON.stringify(x)));
-              });
-            }}
-          >
-            Alert /api/links
-          </a>
-        </p>
-      </header>
+      <UrlShortner />
     </div>
   );
 }
